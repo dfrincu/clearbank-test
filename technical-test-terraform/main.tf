285601c683a169f66d2cd66cdfd14c0094835702
dfrincu <daniel@frincu.org>
2023-12-08 17:15:08 +0000

    switch to for_each

--- technical-test-terraform/main.tf
+++ technical-test-terraform/main.tf
@@ -1,11 +1,9 @@
 locals {
-  service_buses = flatten([
+  service_buses = { for sb in flatten([
     for environment in tolist([var.test, var.staging, var.production]) : [
       for service_bus_name, service_bus in environment.service_buses : {
-        environment          = environment.name
-        resource_group_name  = "rg-${service_bus_name}-${environment.name}"
-        management_lock_name = "ml-${service_bus_name}-${environment.name}"
-        namespace            = "sbns-${service_bus_name}-${environment.name}"
+        name        = service_bus_name
+        environment = environment.name
         tags = {
           cost_centre  = service_bus.cost_centre
           product_name = service_bus.product_name
@@ -13,46 +11,47 @@ locals {
         queues = service_bus.queues
       }
     ]
-  ])
+  ]) : "${sb.name}-${sb.environment}" => sb }
 
-  queues = flatten([
-    for index, service_bus in local.service_buses : [
+  queues = { for q in flatten([
+    for service_bus_key, service_bus in local.service_buses : [
       for queue in service_bus.queues : {
-        sb_index = index
-        name              = "sbq-${queue}-${service_bus.environment}"
+        service_bus = service_bus_key
+        name        = queue
+        environment = service_bus.environment
       }
     ]
-  ])
+  ]) : "${q.name}-${q.service_bus}" => q }
 }
 
 resource "azurerm_resource_group" "rg" {
-  count = length(local.service_buses)
+  for_each = local.service_buses
 
   location = "West Europe"
-  name     = local.service_buses[count.index].resource_group_name
+  name     = "rg-${each.key}"
 }
 
 resource "azurerm_management_lock" "rg_lock" {
-  count = length(local.service_buses)
+  for_each = local.service_buses
 
+  name       = "ml-${each.key}"
+  scope      = azurerm_resource_group.rg[each.key].id
   lock_level = "CanNotDelete"
-  name       = local.service_buses[count.index].management_lock_name
-  scope      = azurerm_resource_group.rg[count.index].id
 }
 
 resource "azurerm_servicebus_namespace" "sbn" {
-  count = length(local.service_buses)
+  for_each = local.service_buses
 
-  name                = local.service_buses[count.index].namespace
-  location            = azurerm_resource_group.rg[count.index].location
-  resource_group_name = azurerm_resource_group.rg[count.index].name
+  name                = "sbns-${each.key}"
+  location            = azurerm_resource_group.rg[each.key].location
+  resource_group_name = azurerm_resource_group.rg[each.key].name
   sku                 = "Standard"
-  tags                = local.service_buses[count.index].tags
+  tags                = each.value.tags
 }
 
 resource "azurerm_servicebus_queue" "queue" {
-  count = length(local.queues)
+  for_each = local.queues
 
-  name         = local.queues[count.index].name
-  namespace_id = azurerm_servicebus_namespace.sbn[local.queues[count.index].sb_index].id
+  name         = "sbq-${each.value.name}-${each.value.environment}"
+  namespace_id = azurerm_servicebus_namespace.sbn[each.value.service_bus].id
 }
--- technical-test-terraform/tests_1.tftest.hcl
+++ technical-test-terraform/tests_1.tftest.hcl
@@ -15,32 +15,32 @@ run "should_provision_resources_with_correct_names" {
   command = plan
 
   assert {
-    condition     = azurerm_resource_group.rg[0].name == "rg-namespace-1-test"
+    condition     = azurerm_resource_group.rg["namespace-1-test"].name == "rg-namespace-1-test"
     error_message = "Resource Group name did not match expected"
   }
 
   assert {
-    condition     = azurerm_management_lock.rg_lock[0].name == "ml-namespace-1-test"
+    condition     = azurerm_management_lock.rg_lock["namespace-1-test"].name == "ml-namespace-1-test"
     error_message = "Management Lock name did not match expected"
   }
 
   assert {
-    condition     = azurerm_servicebus_namespace.sbn[0].name == "sbns-namespace-1-test"
+    condition     = azurerm_servicebus_namespace.sbn["namespace-1-test"].name == "sbns-namespace-1-test"
     error_message = "Service Bus Namespace did not match expected"
   }
 
   assert {
-    condition     = azurerm_servicebus_namespace.sbn[0].tags.cost_centre == "cc-1"
+    condition     = azurerm_servicebus_namespace.sbn["namespace-1-test"].tags.cost_centre == "cc-1"
     error_message = "Service Bus Cost Centre Tag did not match expected"
   }
 
   assert {
-    condition     = azurerm_servicebus_namespace.sbn[0].tags.product_name == "pn-1"
+    condition     = azurerm_servicebus_namespace.sbn["namespace-1-test"].tags.product_name == "pn-1"
     error_message = "Service Bus Product Name Tag did not match expected"
   }
 
   assert {
-    condition     = azurerm_servicebus_queue.queue[0].name == "sbq-some-queue-test"
+    condition     = azurerm_servicebus_queue.queue["some-queue-namespace-1-test"].name == "sbq-some-queue-test"
     error_message = "Queue name did not match expected"
   }
 }
